063dee89c51e6d3c1a7bb6e05a1b77eb2bd30532
andrewhunt628 <andrewhunt628@gmail.com>
2014-12-14 21:55:12 -0600

    Color changes

---
+++ routes/views/challenge.js
@@ -0,0 +1,12 @@
+var keystone = require('keystone'),
+	async = require('async');
+
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res),
+		locals = res.locals;	
+	
+	// Render the view
+	view.render('challenge');
+	
+};
